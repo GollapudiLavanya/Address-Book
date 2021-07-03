87ae0f11a59a0fbb5e91674c978a34c13532dae8
GollapudiLavanya <lavanyagollapudi5166@gmail.com>
2021-07-03 22:23:48 +0530

    [LAVANYA]Addressbook0

---
+++ Address-Book/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book
+{
+    class Contact
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public int Zip;
+        public long PhoneNumber;
+        public string Email;
+    }
+}
