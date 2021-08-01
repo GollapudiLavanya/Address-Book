5d396595e82dc7a35dd66889a706f3ec1081a08b
GollapudiLavanya <lavanyagollapudi5166@gmail.com>
2021-08-01 20:14:55 +0530

    [LAVANYA]Read or write into CSV File

--- Address-Book/FileOperation.cs
+++ Address-Book/FileOperation.cs
@@ -1,10 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Address_Book
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using Newtonsoft.Json;
+namespace AddressBook
 {
     class FileOperation
     {
+        public static void ReadFromFile(string bookName)
+        {
+            string filePath = $"C://Users//dell//source//repos//Address-Book//Address-Book//FileIO//{bookName}.txt";
+            string lines = File.ReadAllText(filePath);
+            Console.WriteLine(lines);
+        }
+        public static void WriteInToFile(string bookName, ContactBook contactBook)
+        {
+            try
+            {
+                string filePath = $"C://Users//dell//source//repos//Address-Book//Address-Book//BinaryFile//{bookName}.csv";
+                StreamWriter sw = new StreamWriter(filePath);
+                foreach (ContactDetails list in contactBook.contactList)
+                {
+                    sw.WriteLine("Name:" + list.firstName + " " + list.lastName + " Address:" + list.address + " City:" + list.city + " State:" + list.state + " Zipcode:" + list.zipcode + " Ph.No:" + list.phonenumber + " Email:" + list.email);
+                }
+                sw.Close();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //Read Or Write into CSV File
+        public static void ReadFromCSVFile(string bookName)
+        {
+            string filePath = $"C://Users//dell//source//repos//Address-Book//Address-Book//CSVFile//{bookName}.csv";
+            string lines = File.ReadAllText(filePath);
+            Console.WriteLine(lines);
+        }
+        public static void WriteIntoCSVFile(string bookName, ContactBook contactBook)
+        {
+            string path = ($"C://Users//dell//source//repos//Address-Book//Address-Book//CSVFile//{bookName}.csv");
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvWriter.WriteField(contactBook.contactList);
+                }
+            }
+        }
     }
 }
